e67a7a61c70a3092759cc50f70e16e9b20a4e4cd
Aldo Santos <aldoacesso@gmail.com>
2026-09-22 21:44:19 -0300

    feat: Setpu iniciado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+////https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <strong>Nutridos</strong>
+      </header>
+    </div>
+  );
+}
+
+export default App;
