088a776fb3e00a1cdea4cbcbef94806513c1252d
Aldo Santos <aldoacesso@gmail.com>
2026-09-22 21:47:11 -0300

    feat: adicionado useState e useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 ////https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        });
+    }
+
+    loadApi();
+  }, []);
   return (
     <div className="container">
       <header>
